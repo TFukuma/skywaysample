9692263bb9e7559c2d2a08a5127148007d12dbe4
TFukuma <36655813+TFukuma@users.noreply.github.com>
2018-06-01 09:34:32 +0900

    Update script.js

--- examples/tutorial/script.js
+++ examples/tutorial/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+let peer = null;
+
+peer = new Peer({
+    key: '746b3de7-c25b-427b-9f83-ba05143046c6',
+    debug: 3
+});
+
+peer.on('open', function(){
+    $('#my-id').text(peer.id);
+});
+
+peer.on('error', function(err){
+    alert(err.message);
+});
+
+peer.on('close', function(){
+});
+
+peer.on('disconnected', function(){
+});
+
+$('#make-call').submit(function(e){
+    e.preventDefault();
+    const call = peer.call($('#callto-id').val(), localStream);
+    setupCallEventHandlers(call);
+});
